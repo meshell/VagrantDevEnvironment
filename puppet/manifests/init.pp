5a974a440bd61134e0fccd9ea63f31a7e41ce389
Michel Estermann <michel.estermann@bbv.ch>
2013-06-26 21:40:17 +0200

    Refactor to puppet style guide.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -11,79 +11,78 @@ $git_author_name = 'Michel Estermann'
 $git_author_email = 'estermann.michel@gmail.com'
 
 
-  exec { 'apt-get update':
-    path => "/usr/bin/",
-    command => 'apt-get update',
-  }
+exec {'apt-get update':
+  path       => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+  command => 'apt-get update',
+}
 
-package { 'git':
-    ensure => 'installed',
+package {'git':
+  ensure    => 'installed',
 }
 
 # Configure Git
-exec { 'git-author-name':
-   command => "/usr/bin/git config --global user.name $git_author_name",
-   require  => Package['git'],
-   unless => "/usr/bin/git config --global --get user.name|grep '$git_author_name'",
+exec {'git-author-name':
+  command => "/usr/bin/git config --global user.name '${git_author_name}'",
+  require    => Package['git'],
+  unless    => "/usr/bin/git config --global --get user.name|grep '${git_author_name}'",
 }
 
-exec { 'git-author-email':
-    command => "/usr/bin/git config --global user.email $git_author_email",
-    require  => Package['git'],
-    unless => "/usr/bin/git config --global --get user.email|grep '$git_author_email'",
+exec {'git-author-email':
+  command => "/usr/bin/git config --global user.email '${git_author_email}'",
+  require    => Package['git'],
+  unless    => "/usr/bin/git config --global --get user.email|grep '{$git_author_email}'",
 }
 
-package { 'cucumber':
-    ensure => 'installed',
-    provider => 'gem',
+package {'cucumber':
+  ensure => 'installed',
+  provider => 'gem',
 }
 
-package { 'cppcheck':
-    ensure => 'installed',
+package {'cppcheck':
+  ensure => 'installed',
 }
 
-package { 'subversion':
-    ensure => 'installed',
+package {'subversion':
+  ensure => 'installed',
 }
 
-package { 'firefox':
-    ensure => 'installed',
+package {'firefox':
+  ensure => 'installed',
 }
-package { 'dbus-x11':
-    ensure => 'installed',
-}
-
-package { 'qtcreator':
-    ensure => 'installed',
-    require  => Package['dbus-x11'],
+package {'dbus-x11':
+  ensure => 'installed',
 }
 
-group {"$user_developer":
+package {'qtcreator':
+  ensure => 'installed',
+  require  => Package['dbus-x11'],
+}
+
+group {"${user_developer}":
   ensure => present,
 }
 
-user { "$user_developer":
-  ensure => present,
-  comment => "Developer",
-  gid => $user_developer,
-  shell => "/bin/bash",
-  home => "/home/$user_developer",
+user {"${user_developer}":
+  ensure        => present,
+  comment      => 'Developer',
+  gid              => $user_developer,
+  shell            => '/bin/bash',
+  home           => "/home/${user_developer}",
   managehome => true,
-  password => $user_pwd,
-  require => Group["$user_developer"],
+  password      => $user_pwd,
+  require          => Group["${user_developer}"],
 }
 
-
-vcsrepo {"/home/$user_developer/$repo_name":
-    ensure   => present,
+vcsrepo {"/home/$user_developer/${repo_name}":
+    ensure => present,
     provider => git,
-    source   => $repo_url,
+    source  => $repo_url,
     revision => 'master',
-    require => Package['git'],
-    user => "$user_developer",
+    require  => Package['git'],
+    user      => $user_developer,
 }
 
 # Dependencies
 
-Exec["apt-get update"] -> Package <| |>
+Exec['apt-get update'] -> Package <| |>
 User['developer']  -> Vcsrepo <| |>
